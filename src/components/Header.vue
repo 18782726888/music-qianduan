9fa68c303d189f5d2c9741d0aa9f51db3d72ad4f
lukaixiong <18782726888@139.com>
2020-04-23 11:51:08 +0800

    创建项目空脚手架提交

---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class='heaser'>
+        <my-header title="新视听" fixed>
+
+        </my-header>
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//引入mint组件库
+import MintUI from 'mint-ui'
+//引入mint样式表
+import "mint-ui/lib/style.min.css";
 
+  Vue.use(MintUI)
 Vue.config.productionTip = false
 
 new Vue({
